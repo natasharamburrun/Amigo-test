29e30dd88ab563dfd12046cd2f771161f872043f
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-09-06 19:51:24 +0100

    updated index img url and title

--- controllers/tmdb.js
+++ controllers/tmdb.js
@@ -1,5 +1,4 @@
 const rp = require('request-promise');
-// const request = require('request');
 
 function indexMovies(req, res) {
   return rp({
@@ -7,11 +6,9 @@ function indexMovies(req, res) {
     url: 'https://api.themoviedb.org/3/discover/movie?api_key=58ae7c2490643e98de044b22b8abac1b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
     json: true,
     body: '{}'
-
   })
     .then(data => res.json(data));
 }
-
 
 function showMovies(req, res) {
   return rp({
--- src/components/indexMovies.js
+++ src/components/indexMovies.js
@@ -8,7 +8,6 @@ class indexMovies extends React.Component {
     this.state = {
       moviesList: []
     };
-    console.log(this.state.body);
   }
 
   componentWillMount() {
@@ -20,22 +19,51 @@ class indexMovies extends React.Component {
       });
   }
 
-
   render() {
+    console.log(this.state.moviesList);
+    const imgURL= 'https://image.tmdb.org/t/p/w500/';
     return (
-      <main>
-        <h1>Movies Index Page</h1>
-        {this.state.moviesList ? (
-          this.state.moviesList.map(movie =>
-            <h2 key={movie.id}>{movie.title}</h2>
-          )
-        ) : (
-          <h2>Loading... </h2>
-        )}
-      </main>
+      <section className="section">
+        <div className="container-index">
+          <div className="Title">
+            <h1>Movies Index Page</h1>
+          </div>
+          <div className="columns is-multiline">
+            {this.state.moviesList.map(movie =>
+              <div key={movie.id} className="column is-one-quarter-desktop is-half-tablet">
+
+                <div className="card-movie">
+                  <div className="content">
+                    <h2 className="title">{movie.title}</h2>
+                  </div>
+                  <figure className="image">
+                    <img src= {`${imgURL}${movie.poster_path}`} />
+                  </figure>
+                </div>
+
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+
+      //
+      //       <main>
+      //         <div >
+      //           <h1>Movies Index Page</h1>
+      //           {this.state.moviesList ? (
+      //             this.state.moviesList.map(movie =>
+      //               <h2 key={movie.id}>{movie.title}</h2>
+      //             )
+      //           ) : (
+      //             <h2>Loading... </h2>
+      //           )}
+      //         </div>
+      //       </main>
+      //     );
+      //   }
+      //
     );
   }
-
 }
-
 export default indexMovies;
